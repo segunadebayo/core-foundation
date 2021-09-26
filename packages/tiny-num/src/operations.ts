8298440d3709c4f21628f9ebaec30853d905b5f2
Segun Adebayo <joseshegs@gmail.com>
2021-09-27 02:30:58 +0400

    chore: update operations

--- packages/tiny-num/src/operations.ts
+++ packages/tiny-num/src/operations.ts
@@ -2,9 +2,9 @@ import { countDecimals, parseIntl } from "./base"
 import type { Num } from "./types"
 
 export const clamp = (v: number, o: Num<"min" | "max">) => Math.min(Math.max(v, o.min), o.max)
-export const inc = (v: number, s: number) => parseIntl(v + s)
-export const dec = (v: number, s: number) => parseIntl(v - s)
-export const div = (v: number, s: number) => parseIntl(v / s)
+export const increment = (v: number, s: number) => parseIntl(v + s)
+export const decrement = (v: number, s: number) => parseIntl(v - s)
+export const multiply = (v: number, s: number) => parseIntl(v * s)
 
 export function round(v: number, t?: number) {
   const p = 10 ** (t ?? 10)
